01eae81dde0318871d571f2c2451aa43909229c0
Michael Dorst <m@mdorst.net>
2020-05-11 19:08:13 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+  public float inputAccel;
+  public Rigidbody2D rigidBody;
+  Vector2 input;
+  Vector2 momentum;
+
+  void Update()
+  {
+    input.x = Input.GetAxisRaw("Horizontal");
+    input.y = Input.GetAxisRaw("Vertical");
+  }
+
+  void FixedUpdate()
+  {
+    momentum.x += input.x * inputAccel * Time.fixedDeltaTime;
+    momentum.y += input.y * inputAccel * Time.fixedDeltaTime;
+    rigidBody.MovePosition(rigidBody.position + momentum);
+  }
+}
